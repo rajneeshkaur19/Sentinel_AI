dcf2c9a82df12c780bb10c5049be89a6d1807147
Rajneesh Kaur <rajneesh19f@gmail.com>
2026-07-14 14:18:41 +0530

    feat: create reusable sidebar navigation with routing

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -1,20 +1,21 @@
-import { NavLink } from "react-router-dom";
+import SidebarItem from "./ SidebarItem";
 
 function Sidebar() {
   return (
-    <aside className="w-64 min-h-screen bg-slate-900 text-white p-6">
-      <h1 className="text-2xl font-bold mb-8">
+    <aside className="w-64 min-h-screen bg-slate-900 text-white p-6 shadow-xl">
+      <h1 className="text-3xl font-bold mb-10 text-cyan-400">
         🛡 SentinelAI
       </h1>
 
-      <nav className="flex flex-col gap-4">
-        <NavLink to="/">Dashboard</NavLink>
-        <NavLink to="/malware-scanner">Malware Scanner</NavLink>
-        <NavLink to="/threat-intel">Threat Intelligence</NavLink>
-        <NavLink to="/ai-assistant">AI Assistant</NavLink>
-        <NavLink to="/reports">Reports</NavLink>
-        <NavLink to="/settings">Settings</NavLink>
-        <NavLink to="/url-scanner">URL Scanner</NavLink>
+      <nav className="flex flex-col gap-3">
+        <SidebarItem title = "Dashboard" to = "/" />
+        <SidebarItem title = "URL Scanner" to = "/url-scanner" />
+        <SidebarItem title = "Malware Scanner" to = "/malware-scanner" />
+        <SidebarItem title = "Threat Intelligence" to = "/threat-intel" />
+        <SidebarItem title = "AI Assistant" to = "/ai-assistant" />
+        <SidebarItem title = "Reports" to = "/reports" />
+        <SidebarItem title = "Settings" to = "/settings" />
+
       </nav>
     </aside>
   );
---
+++ frontend/src/components/sidebar/SidebarItem.jsx
@@ -0,0 +1,22 @@
+import { NavLink } from
+"react-router-dom";
+function SidebarItem({ title, to}){
+    return(
+        <NavLink
+           to = {to}
+           classname = {({ isActive }) =>
+            `block rounded-lg px-4 py-3 transition-all duration-200 ${
+                isActive 
+                ? "bg-cyan-500 text-white font-semibold"
+                : "text-slate-300 hover:bg-slate-800 hover:text-white"
+            
+            }`
+        }    
+    >
+        {title}
+        </NavLink>
+
+    );
+}
+
+export default SidebarItem;
